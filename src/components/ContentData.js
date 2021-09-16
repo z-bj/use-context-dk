3c0d2e5b4dd11b62b729f1e34f2871ca72df0b21
Bj'z <zbjarchi@gmail.com>
2021-09-16 11:18:41 +0100

    with useContext

--- src/components/ContentData.js
+++ src/components/ContentData.js
@@ -1,25 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { UserContext, ColorContext } from "./MyContext";
+
 const ContentData = () => {
+  const user = useContext(UserContext);
+  const color = useContext(ColorContext);
+
   return (
-    <UserContext.Consumer>
-      {(user) => {
-        return (
-          <ColorContext.Consumer>
-            {(color) => {
-              return (
-                <div style={{ color: color }}>
-                  <ul>
-                    <li>Nom:{user.name}</li>
-                    <li>Age:{user.Age}</li>
-                  </ul>
-                </div>
-              );
-            }}
-          </ColorContext.Consumer>
-        );
-      }}
-    </UserContext.Consumer>
+    <div style={{ color: color }}>
+      <ul>
+        <li>Nom:{user.name}</li>
+        <li>Age:{user.Age}</li>
+      </ul>
+    </div>
   );
 };
 
